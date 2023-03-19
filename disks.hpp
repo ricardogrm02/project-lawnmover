110952fcee17ebff7450ec8340c9140fdaf881a0
ricardogrm02 <ricardog2002@csu.fullerton.edu>
2023-03-18 20:14:19 -0700

    Project Submission

--- disks.hpp
+++ disks.hpp
@@ -11,26 +11,23 @@
 
 #pragma once
 
-
-
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <functional>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <functional>
-#include <iostream>
 
-enum disk_color { DISK_LIGHT, DISK_DARK};
+enum disk_color { DISK_LIGHT, DISK_DARK };
 
 class disk_state {
 private:
   std::vector<disk_color> _colors;
 
 public:
-  disk_state(size_t light_count)
-    : _colors(light_count * 2, DISK_LIGHT) {
+  disk_state(size_t light_count) : _colors(light_count * 2, DISK_LIGHT) {
 
     assert(light_count > 0);
 
@@ -39,25 +36,17 @@ public:
     }
   }
 
-  bool operator== (const disk_state& rhs) const {
+  bool operator==(const disk_state &rhs) const {
     return std::equal(_colors.begin(), _colors.end(), rhs._colors.begin());
   }
 
-  size_t total_count() const {
-    return _colors.size();
-  }
+  size_t total_count() const { return _colors.size(); }
 
-  size_t light_count() const {
-    return total_count() / 2;
-  }
+  size_t light_count() const { return total_count() / 2; }
 
-  size_t dark_count() const {
-    return light_count();
-  }
+  size_t dark_count() const { return light_count(); }
 
-  bool is_index(size_t i) const {
-    return (i < total_count());
-  }
+  bool is_index(size_t i) const { return (i < total_count()); }
 
   disk_color get(size_t index) const {
     assert(is_index(index));
@@ -94,26 +83,38 @@ public:
   // that the first disk at index 0 is light, the second disk at index 1
   // is dark, and so on for the entire row of disks.
   bool is_initialized() const {
-      for (size_t i = 0; i < total_count(); i++){     //check each position before function
-        if (i % 2 == 0){                              //check even position --> should be light
-            if (_colors[i] == DISK_DARK) {
-              return false;
-            }
-        } else {                                      //check odd position --> should be dark
-            if (_colors[i] == DISK_LIGHT) {
-              return false;
-            }
+    for (size_t i = 0; i < total_count();
+         i++) {         // check each position before function
+      if (i % 2 == 0) { // check even position --> should be light
+        if (_colors[i] == DISK_DARK) {
+          return false;
+        }
+      } else { // check odd position --> should be dark
+        if (_colors[i] == DISK_LIGHT) {
+          return false;
         }
       }
+    }
 
-      return true;
+    return true;
   }
 
   // Return true when this disk_state is fully sorted, with all light disks on
   // the left (low indices) and all dark disks on the right (high indices).
   bool is_sorted() const {
-      
-      return true;
+    // Iterate through the vector containing disk_state
+    for (int i = 0; i < total_count(); i++) {
+      // Check if we have dark disk in the lower indices, return false if we do
+      if (get(i) == DISK_DARK && i < total_count() / 2) {
+        return false;
+      } // Check if we have dark disk in the lower indices, return false if we
+        // do
+      else if (get(i == DISK_LIGHT && i > total_count() / 2)) {
+        return false;
+      }
+    }
+    // We didn't find any disk out of place, return true
+    return true;
   }
 };
 
@@ -126,36 +127,78 @@ private:
   unsigned _swap_count;
 
 public:
+  sorted_disks(const disk_state &after, unsigned swap_count)
+      : _after(after), _swap_count(swap_count) {}
 
-  sorted_disks(const disk_state& after, unsigned swap_count)
-    : _after(after), _swap_count(swap_count) { }
+  sorted_disks(disk_state &&after, unsigned swap_count)
+      : _after(after), _swap_count(swap_count) {}
 
-  sorted_disks(disk_state&& after, unsigned swap_count)
-    : _after(after), _swap_count(swap_count) { }
+  const disk_state &after() const { return _after; }
 
-  const disk_state& after() const {
-    return _after;
-  }
-
-  unsigned swap_count() const {
-    return _swap_count;
-  }
+  unsigned swap_count() const { return _swap_count; }
 };
 
 // Algorithm that sorts disks using the alternate algorithm.
-sorted_disks sort_alternate(const disk_state& before) {
-	int numOfSwap = 0;                                                                      //record # of step swap
- 
-          }
+sorted_disks sort_alternate(const disk_state &before) {
+  int numOfSwap = 0;
+  disk_state disks = before;
+  // Run loop for every disk in the vector
+  for (int i = 0; i < disks.total_count() - 1; i++) {
+    // if i is even, then we iterate through every even pair
+    if (i % 2 == 0) {
+      // Iterate through each even pair
+      for (int j = 0; j < disks.total_count() - 1; j += 2) {
+        // if left disk is dark and right disk is light swap, increment #ofSwaps
+        if (disks.get(j) == DISK_DARK && disks.get(j + 1) == DISK_LIGHT) {
+          disks.swap(j);
+          numOfSwap++;
+        }
+      }
+    } // if i is odd iterate through the odd pairs
+    else if (i % 2 == 1) {
+      // iterate through the odd pairs
+      for (int k = 1; k < disks.total_count() - 1; k += 2) {
+        // if left  disk is dark and right disk is light swap, increment
+        // #ofSwaps
+        if (disks.get(k) == DISK_DARK && disks.get(k + 1) == DISK_LIGHT) {
+          disks.swap(k);
+          numOfSwap++;
+        }
+      }
+    }
+  }
 
-  return sorted_disks(disk_state(state), numOfSwap);
+  // record # of step swap
+  return sorted_disks(disk_state(disks), numOfSwap);
 }
 
-
 // Algorithm that sorts disks using the lawnmower algorithm.
-sorted_disks sort_lawnmower(const disk_state& before) {
-  	
-	  }
+sorted_disks sort_lawnmower(const disk_state &before) {
+  int numOfSwap = 0;
+  disk_state disks = before;
+  // iterate through the vector n/2 times (light_count = n/2)
+  for (int i = 0; i < disks.light_count();
+       i++) { // lightcount is just totalcount/2
 
-  return sorted_disks(disk_state(state), numOfSwap);
+    // Iterate through the vector from left to right
+    for (int j = 0; j < disks.total_count() - 1; j++) {
+      // if the disk on the left is dark and the disk on the right is light,
+      // swap them, increment num of swaps
+      if (disks.get(j) == DISK_DARK && disks.get(j + 1) == DISK_LIGHT) {
+        disks.swap(j);
+        numOfSwap++;
+      }
+    }
+    // Iterate through the vector from right to left
+    for (int k = disks.total_count() - 1; k > 0; k--) {
+      // if the disk on the right is a light one, and the one on the left is
+      // dark, swap them, increment num of swap
+      if (disks.get(k) == DISK_LIGHT && disks.get(k - 1) == DISK_DARK) {
+        disks.swap(k - 1);
+        numOfSwap++;
+      }
+    }
+  }
+
+  return sorted_disks(disk_state(disks), numOfSwap);
 }
